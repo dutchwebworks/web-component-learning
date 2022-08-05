a3c3b068b0828083cd7799f6cb5bf6862a05fb9b
Dennis Burger <dennis@dutchwebworks.nl>
2022-08-05 15:40:42 +0200

    Refactored to basic example

--- weather-app.js
+++ weather-app.js
@@ -1,25 +1,20 @@
-// https://opensource.com/article/21/7/web-components
-
-const weatherAppTemplate = document.createElement("template");
-
-weatherAppTemplate.innerHTML = `
-  <div class="weather-app">
-    <fieldset class="weather-app__body">
-        <legend>Today's weather</legend>
-
-        <p>Pretty hot today</p>
-
-        <slot />
-    </fieldset>
-  </div>
-`;
-
 class WeatherApp extends HTMLElement {
-    constructor() {
-        super();
-        this._shadowRoot = this.attachShadow({ mode: "open" });
-        this._shadowRoot.appendChild(weatherAppTemplate.content.cloneNode(true));
-    }
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+
+    this.shadowRoot.innerHTML = `
+      <div class="weather-app">
+        <fieldset class="weather-app__body">
+            <legend class="weather-app__title">Today's weather</legend>
+
+            <p>Pretty hot today</p>
+
+            <slot class="weahter-app__slot" />
+        </fieldset>
+      </div>
+    `;
+  }
 }
 
-window.customElements.define("weather-app", WeatherApp);
+customElements.define("weather-app", WeatherApp);
